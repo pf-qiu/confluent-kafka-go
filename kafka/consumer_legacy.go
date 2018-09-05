08ec83026c90c2c6e1da2408cdab6c631f00b966
Peifeng Qiu <pqiu@pivotal.io>
2018-09-05 15:20:49 +0800

    Add batch mode to legacy consumer

--- kafka/consumer_legacy.go
+++ kafka/consumer_legacy.go
@@ -8,6 +8,38 @@ import (
 /*
 #include <librdkafka/rdkafka.h>
 #include <stdlib.h>
+
+typedef struct message_contiainer
+{
+	rd_kafka_message_t** msgs;
+	size_t count;
+} message_contiainer;
+
+void init_container(message_contiainer* container, size_t count)
+{
+	container->count = count;
+	container->msgs = malloc(sizeof(rd_kafka_message_t*) * count);
+}
+
+void free_container_messages(message_contiainer* container)
+{
+	free(container->msgs);
+}
+
+ssize_t consume_messages(rd_kafka_topic_t *rkt, int32_t partition,
+				int timeout_ms, message_contiainer* container)
+{
+	return rd_kafka_consume_batch(rkt, partition, timeout_ms,
+		container->msgs, container->count);
+}
+
+void destroy_messages(rd_kafka_message_t **msgs, ssize_t count)
+{
+	for (ssize_t i = 0; i < count; i++)
+	{
+		rd_kafka_message_destroy(msgs[i]);
+	}
+}
 */
 import "C"
 
@@ -16,6 +48,7 @@ type LegacyConsumer struct {
 	rkt       *C.rd_kafka_topic_t
 	partition int32
 	topic     string
+	container C.message_contiainer
 }
 
 // Strings returns a human readable name for a Consumer instance
@@ -28,6 +61,10 @@ func (c *LegacyConsumer) Close() error {
 		C.rd_kafka_consumer_close(c.rk)
 	}
 	C.rd_kafka_destroy(c.rk)
+
+	if c.container.count > 0 {
+		C.free_container_messages(&c.container)
+	}
 	return nil
 }
 
@@ -87,6 +124,34 @@ func (c *LegacyConsumer) Poll(timeoutMs int) (event Event) {
 	return nil
 }
 
+func (c *LegacyConsumer) PollBatch(timemoutMs int) []Event {
+	if c.rkt == nil {
+		return nil
+	}
+
+	if c.container.count <= 0 {
+		return nil
+	}
+	C.rd_kafka_poll(c.rk, C.int(0))
+
+	count := C.consume_messages(c.rkt, C.int32_t(c.partition), C.int(timemoutMs), &c.container)
+	if count > 0 {
+		defer C.destroy_messages(c.container.msgs, count)
+		messages := c.getMessage(int(count))
+		events := make([]Event, count)
+		for i := 0; i < int(count); i++ {
+			events[i] = c.buildMessage(messages[i])
+		}
+		return events
+	}
+	return nil
+}
+
+func (c *LegacyConsumer) getMessage(count int) []*C.rd_kafka_message_t {
+	slice := (*[1 << 30]*C.rd_kafka_message_t)(unsafe.Pointer(c.container.msgs))[:count:count]
+	return slice
+}
+
 func (c *LegacyConsumer) buildMessage(cmsg *C.rd_kafka_message_t) Event {
 	if cmsg.err == 0 {
 		msg := &Message{}
@@ -134,6 +199,33 @@ func NewLegacyConsumer(conf *ConfigMap) (*LegacyConsumer, error) {
 	if c.rk == nil {
 		return nil, newErrorFromCString(C.RD_KAFKA_RESP_ERR__INVALID_ARG, cErrstr)
 	}
+
+	return c, nil
+}
+
+func NewLegacyBatchConsumer(conf *ConfigMap, batchSize int) (*LegacyConsumer, error) {
+	err := versionCheck()
+	if err != nil {
+		return nil, err
+	}
+
+	c := &LegacyConsumer{}
+
+	cConf, err := conf.convert()
+	if err != nil {
+		return nil, err
+	}
+	cErrstr := (*C.char)(C.malloc(C.size_t(256)))
+	defer C.free(unsafe.Pointer(cErrstr))
+
+	C.rd_kafka_conf_set_events(cConf, C.RD_KAFKA_EVENT_REBALANCE|C.RD_KAFKA_EVENT_OFFSET_COMMIT|C.RD_KAFKA_EVENT_STATS)
+
+	c.rk = C.rd_kafka_new(C.RD_KAFKA_CONSUMER, cConf, cErrstr, 256)
+	if c.rk == nil {
+		return nil, newErrorFromCString(C.RD_KAFKA_RESP_ERR__INVALID_ARG, cErrstr)
+	}
+
+	C.init_container(&c.container, C.size_t(batchSize))
 
 	return c, nil
 }
